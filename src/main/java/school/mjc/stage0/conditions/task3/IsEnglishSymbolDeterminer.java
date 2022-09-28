deae2714a057ef38e2b8e011a62930e05645652c
Phobos027 <shkuro.stepan@gmail.com>
2022-09-28 14:30:01 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -3,6 +3,6 @@ package school.mjc.stage0.conditions.task3;
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
         if (symbol >= 'A' && symbol <= 'z') System.out.println("English");
-        else System.out.println("Non english");
+        else System.out.println("Non English");
     }
 }
