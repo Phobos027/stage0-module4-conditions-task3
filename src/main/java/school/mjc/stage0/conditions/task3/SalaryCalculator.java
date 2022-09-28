f4fceaa0a89b45b4438fe68ec1e7db2e7ad2e088
Phobos027 <shkuro.stepan@gmail.com>
2022-09-28 14:14:07 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double salaryAfterTaxis;
+        if (salary <= 10000 && salary >= 0) salaryAfterTaxis = salary * 0.85;
+        else if (salary > 10000 && salary <= 20000) salaryAfterTaxis = salary * 0.82;
+        else if (salary > 20000) salaryAfterTaxis = salary * 0.8;
+        else System.out.println("wrong input!");
     }
 }
