0a1957909ce26ed6f8c1c8a7ec753a9e0f4a9d0a
Phobos027 <shkuro.stepan@gmail.com>
2022-09-28 14:27:55 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
-        if (character < 'A' && character > 'z') System.out.println("wrong alphabet!");
+        if (character < 'A' || character > 'z') System.out.println("wrong alphabet!");
         else if (character == 'A' || character == 'a' || character == 'E' || character == 'e' ||
                     character == 'I' || character == 'i' || character == 'O' || character == 'o' ||
                         character == 'U' || character == 'u' || character == 'Y' || character == 'y')
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,9 +3,15 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         double salaryAfterTaxis;
-        if (salary <= 10000 && salary >= 0) salaryAfterTaxis = salary * 0.85;
-        else if (salary > 10000 && salary <= 20000) salaryAfterTaxis = salary * 0.82;
-        else if (salary > 20000) salaryAfterTaxis = salary * 0.8;
-        else System.out.println("wrong input!");
+        if (salary <= 10000 && salary >= 0) {
+            salaryAfterTaxis = salary * 0.85;
+            System.out.println(salaryAfterTaxis);
+        } else if (salary > 10000 && salary <= 20000) {
+            salaryAfterTaxis = salary * 0.82;
+            System.out.println(salaryAfterTaxis);
+        } else if (salary > 20000) {
+            salaryAfterTaxis = salary * 0.8;
+            System.out.println(salaryAfterTaxis);
+        } else System.out.println("wrong input!");
     }
 }
